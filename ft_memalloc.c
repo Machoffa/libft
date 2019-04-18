ba231919e31d3071eae3645d80f3723a92caf921
Mathieu CHOFFARDET <machoffa@e1r10p12.42.fr>
2019-04-18 12:38:11 +0200

    libft

---
+++ ft_memalloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: machoffa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/07 15:55:22 by machoffa          #+#    #+#             */
+/*   Updated: 2019/04/15 13:54:01 by machoffa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memalloc(size_t size)
+{
+	void *res;
+
+	if (!(res = (void *)malloc(sizeof(void) * size)))
+		return (NULL);
+	ft_bzero(res, size);
+	return (res);
+}
